30471b0b39b1baf4a094432c527106fcdb1fb1ba
Steve Jenson <stevej@pobox.com>
2010-02-15 20:11:49 -0800

    Simplifying my project layout.

--- project/build/Mobilemeat.scala
+++ project/build/Mobilemeat.scala
@@ -3,16 +3,13 @@ import sbt._
 trait Defaults {
   def androidPlatformName = "android-2.1"
 }
-class Mobilemeat(info: ProjectInfo) extends ParentProject(info) {
+
+class Mobilemeat(info: ProjectInfo) extends AndroidProject(info) with Defaults with MarketPublish {
   override def shouldCheckOutputDirectories = false
   override def updateAction = task { None }
 
-  lazy val main  = project(".", "mobilemeat", new MainProject(_))
-  lazy val tests = project("tests",  "tests", new TestProject(_), main)
-
-  class MainProject(info: ProjectInfo) extends AndroidProject(info) with Defaults {
-    val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test"
-  }
+  val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test"
+  val keyalias = "/Users/stevej/.keystore/saladwithsteve.keystore"
 
   class TestProject(info: ProjectInfo) extends AndroidTestProject(info) with Defaults
 }
