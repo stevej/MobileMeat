b8392d2ecedfddee6794b310b6727371e3979245
Steve Jenson <stevej@pobox.com>
2010-02-15 19:35:35 -0800

    publish for 2.1

--- project/build/Mobilemeat.scala
+++ project/build/Mobilemeat.scala
@@ -1,16 +1,16 @@
 import sbt._
 
 trait Defaults {
-  def androidPlatformName = "android-1.6"
+  def androidPlatformName = "android-2.1"
 }
 class Mobilemeat(info: ProjectInfo) extends ParentProject(info) {
   override def shouldCheckOutputDirectories = false
-  override def updateAction = task { None } 
+  override def updateAction = task { None }
 
   lazy val main  = project(".", "mobilemeat", new MainProject(_))
   lazy val tests = project("tests",  "tests", new TestProject(_), main)
 
-  class MainProject(info: ProjectInfo) extends AndroidProject(info) with Defaults {    
+  class MainProject(info: ProjectInfo) extends AndroidProject(info) with Defaults {
     val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test"
   }
 
