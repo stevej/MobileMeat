05d484736862feb9f4b3d94cd4eaac343131a0a8
Steve Jenson <stevej@fruitless.org>
2011-04-16 21:20:34 -0700

    use a more idiomatic map syntax, kill _root_ as 2.8 has removed the need for it. no more .toList.toArray, you can just call .toArray. wow, scala 2.8 really is much better.

--- src/main/scala/MeatList.scala
+++ src/main/scala/MeatList.scala
@@ -1,29 +1,29 @@
 package com.saladwithsteve.mobilemeat
 
-import _root_.android.app.ListActivity
-import _root_.android.content.Intent
-import _root_.android.os.Bundle
-import _root_.android.net.Uri
-import _root_.android.widget.{ArrayAdapter, ListView, Toast}
-import _root_.android.view.View
+import android.app.ListActivity
+import android.content.Intent
+import android.os.Bundle
+import android.net.Uri
+import android.widget.{ArrayAdapter, ListView, Toast}
+import android.view.View
 
 
 class MeatList extends ListActivity {
-  val brisket = ("Brisket" -> "http://www.flickr.com/photos/azarius/3399436470/sizes/l/")
-  val sausage = ("Sausage" -> "http://www.flickr.com/photos/wrigleywrld/3850003593/sizes/l/")
-  val ribs = ("Ribs" -> "http://www.flickr.com/photos/wrigleywrld/3828383890/sizes/l/in/set-72157622165170836/")
-  val redMeat = ("Tasmanian Steak" -> "http://www.flickr.com/photos/brucewood/535612522/sizes/m/")
-  val duck = ("Red Duck" -> "http://www.flickr.com/photos/75304241@N00/343301651/sizes/l/")
-  val assorted = ("Assorted Meats" -> "http://www.flickr.com/photos/foodnut/3885598840/sizes/o/")
-  val deliMeats = ("Deli Meats" -> "http://www.flickr.com/photos/sifu_renka/2096895740/sizes/l/")
-  val groundBeef = ("Ground Beef" -> "http://www.flickr.com/photos/aventurista/15704030/sizes/o/")
-  val beefJerkey = ("Beef Jerkey" -> "http://www.flickr.com/photos/andreelau/2149549554/sizes/o/")
-  val fineMeat = ("A fine meat" -> "http://www.flickr.com/photos/stefz/20480619/sizes/o/")
-  val meatFace = ("Meat face" -> "http://www.flickr.com/photos/epiglotic/2126582943/sizes/l/")
+  def flickr(subpath: String) = "http://www.flickr.com/photos/" + subpath
 
-  val meatMap = Map.empty ++ List(brisket, sausage, ribs, redMeat, duck, assorted, deliMeats, groundBeef, beefJerkey, fineMeat, meatFace)
+  val meats = Map("Brisket"         -> flickr("azarius/3399436470/sizes/l/"),
+                  "Sausage"         -> flickr("wrigleywrld/3850003593/sizes/l/"),
+                  "Ribs"            -> flickr("wrigleywrld/3828383890/sizes/l/in/set-72157622165170836/"),
+                  "Tasmanian Steak" -> flickr("brucewood/535612522/sizes/m/"),
+                  "Red Duck"        -> flickr("75304241@N00/343301651/sizes/l/"),
+                  "Assorted Meats"  -> flickr("foodnut/3885598840/sizes/o/"),
+                  "Deli Meats"      -> flickr("sifu_renka/2096895740/sizes/l/"),
+                  "Ground Beef"     -> flickr("aventurista/15704030/sizes/o/"),
+                  "Beef Jerkey"     -> flickr("andreelau/2149549554/sizes/o/"),
+                  "A fine meat"     -> flickr("stefz/20480619/sizes/o/"),
+                  "Meat face"       -> flickr("epiglotic/2126582943/sizes/l/"))
 
-  val MEATS: Array[String] = meatMap.keys.toList.toArray
+  val MEATS: Array[String] = meats.keys.toArray
 
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
@@ -38,7 +38,7 @@ class MeatList extends ListActivity {
     // Show a hover notification to make the user feel better about the impending meat.
     Toast.makeText(this, "Now Viewing: " + meat, Toast.LENGTH_LONG).show()
 
-    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(meatMap(meat))))
+    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(meats(meat))))
   }
 }
 
